a7ac690754b28c02e68287be4551be9968ae9c69
AdamMerhi <adam_merhi@icloud.com>
2025-04-23 20:39:06 +1000

    App Setup

---
+++ Interactive Journal/Interactive Journal/Interactive_JournalApp.swift
@@ -0,0 +1,17 @@
+//
+//  Interactive_JournalApp.swift
+//  Interactive Journal
+//
+//  Created by Adam Merhi on 23/4/2025.
+//
+
+import SwiftUI
+
+@main
+struct Interactive_JournalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
