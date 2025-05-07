5bdec3270cdf5e985414478edd4454f59074ccab
AdamMerhi <adam_merhi@icloud.com>
2025-05-07 18:22:21 +1000

    test 1

---
+++ Interactive JournalTests/Interactive_JournalTests.swift
@@ -0,0 +1,17 @@
+//
+//  Interactive_JournalTests.swift
+//  Interactive JournalTests
+//
+//  Created by Adam Merhi on 23/4/2025.
+//
+
+import Testing
+@testable import Interactive_Journal
+
+struct Interactive_JournalTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
