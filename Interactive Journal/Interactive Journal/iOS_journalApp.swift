8cb6dc0fc55a55adbc35b3440cac4012a37a62e3
justcharchar <charlizetahimic@gmail.com>
2025-05-10 17:47:52 +1000

    my first part - landing page
    code for the landing page

---
+++ Interactive Journal/Interactive Journal/iOS_journalApp.swift
@@ -0,0 +1,17 @@
+//
+//  iOS_journalApp.swift
+//  iOS journal
+//
+//  Created by Ceri Tahimic on 9/5/2025.
+//
+
+import SwiftUI
+
+@main
+struct iOS_journalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            LandingView()
+        }
+    }
+}
